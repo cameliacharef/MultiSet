73f1758459e626cee440e89322b99a59f6abaea3
Antoine Miné <antoine.mine@lip6.fr>
2024-09-26 17:03:54 +0200

    Initial commit

---
+++ src/pobj/multiset/test/HashMultiSetTest.java
@@ -0,0 +1,27 @@
+package pobj.multiset.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import pobj.multiset.HashMultiSet;
+import pobj.multiset.MultiSet;
+
+public class HashMultiSetTest {
+	
+	private MultiSet<String> m;
+	
+	@BeforeEach
+	public void before() {
+		m = new HashMultiSet<>();
+		m.add("a");
+		m.add("a",5);
+		m.add("b",3);
+	}
+	
+	@Test 
+	public void test1() {
+		assertEquals(6, m.count("a"));
+	}
+}
