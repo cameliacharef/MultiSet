aef78c1482ac083e8f6ce914296a74dad6c61f01
camelia <camelia@debian>
2024-10-17 22:52:26 +0200

    test

--- src/pobj/multiset/test/MultiSetParserTest.java
+++ src/pobj/multiset/test/MultiSetParserTest.java
@@ -1,7 +1,19 @@
 package pobj.multiset.test;
 
+import pobj.multiset.InvalidMultiSetFormat;
+import pobj.multiset.MultiSet;
+import pobj.multiset.MultiSetParser;
+
 public class MultiSetParserTest {
 	public static void main (String[] args) {
+		String fichier = "data/parse.txt";
+		try {
+			MultiSet<String> multiSet = MultiSetParser.parse(fichier);
+			System.out.println(multiSet);
+		}
+		catch(InvalidMultiSetFormat e) {
+			System.out.println(e.getMessage());
+		}
 		
 	}
 }
